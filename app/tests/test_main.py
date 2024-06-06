b1e6ed4c6ffe7576ccbd1a87ca0fecc969c1a7cf
jeong <jw.jeong@tilda.co.kr>
2024-06-07 00:11:09 +0900

    test code setting

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,19 +1,20 @@
 import pytest
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from collections.abc import AsyncGenerator
-from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from httpx import AsyncClient
 
-from main import app, get_db_session
-from configs.app_config import get_postgres_test_connection
+from app.common.infra.base import Base
+from app.core.database.async_session import get_db_session
+from app.main import app
+from app.configs.app_config import get_postgres_test_connection
 
 TEST_DATABASE_URL = get_postgres_test_connection()
 
 test_engine = create_async_engine(TEST_DATABASE_URL)
 TestingSessionLocal = async_sessionmaker(bind=test_engine, expire_on_commit=False)
 
-Base = declarative_base()
 
 # Override the get_db dependency
 async def override_get_db_session() -> AsyncGenerator[AsyncSession, None]:
@@ -30,20 +31,20 @@ app.dependency_overrides[get_db_session] = override_get_db_session
 @pytest.fixture(scope="module")
 async def client() -> AsyncGenerator[AsyncClient, None]:
     # Create the database tables
-    async with engine.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
 
     # Drop the database tables after the tests
-    async with engine.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_read_root(client: AsyncClient):
     response = await client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"message": "Hello World"}
 
 # Add more test cases as needed
