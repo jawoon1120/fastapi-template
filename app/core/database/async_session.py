853d255a6f859ffea1d32ee6ca0d379d182a00ef
jeong <jw.jeong@tilda.co.kr>
2024-06-08 23:15:39 +0900

    scoped session 설정

--- app/core/database/async_session.py
+++ app/core/database/async_session.py
@@ -4,16 +4,24 @@ from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
+    async_scoped_session
 )
+import asyncio
 
 engine = create_async_engine(get_postgres_connection())
+session_factory = async_sessionmaker(bind=engine, autocommit=False, autoflush=True, expire_on_commit=False)
+
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    
-    factory = async_sessionmaker(engine)
-    async with factory() as session:
+
+    Session = async_scoped_session(session_factory, scopefunc=asyncio.current_task)
+
+    async with Session() as session:
         try:
             yield session
             await session.commit()
         except:
             await session.rollback()
             raise
+        finally:
+            await Session.remove()
+            await engine.dispose()
