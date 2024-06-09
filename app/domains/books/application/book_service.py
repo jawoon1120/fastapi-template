2cf0a4d2ebd29f39b8bc23fa0d808818f5c2477f
jeong <jw.jeong@tilda.co.kr>
2024-06-09 16:10:59 +0900

    transaction 테스트 코드

--- app/domains/books/application/book_service.py
+++ app/domains/books/application/book_service.py
@@ -1,15 +1,28 @@
 from typing import Annotated
 from fastapi import Depends
 
+from app.core.database.async_session import get_db_session
 from app.domains.books.infra.book_entity import BookEntity
 from ..infra.book_repostiory import BookRepository
-
+from sqlalchemy.ext.asyncio import AsyncSession
 
 class BookService:
-    def __init__(self, book_repository = Depends(BookRepository)):
-        self.book_repoistory = book_repository
-    
+    def __init__(
+        self, 
+        book_repository = Depends(BookRepository),
+        session:AsyncSession = Depends(get_db_session)
+    ):
+        self.book_repoistory:BookRepository = book_repository
+        self.session:AsyncSession = session
+
     async def get_books(self) -> list[BookEntity]:
         
         books = await self.book_repoistory.get_books()
         return books
+    
+
+    async def create_book_one_two(self):
+        async with self.session.begin():
+            self.book_repoistory.create_book_one()
+            self.book_repoistory.create_book_two()
+        # return
