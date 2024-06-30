7abf9eddff5c05d421dc9f8c400cba413f52626f
jeong <jw.jeong@tilda.co.kr>
2024-06-30 16:24:06 +0900

    token domain & dependency

--- app/domains/auth/domain/token.py
+++ app/domains/auth/domain/token.py
@@ -2,3 +2,16 @@
 class Token():
     access_token: str
     token_type: str
+
+
+class TokenPayload():
+    user_id: int
+    user_email:str
+
+    def __init__(self, user_id:int, user_email:str):
+        self.user_id = user_id
+        self.user_email = user_email
+
+    def __iter__(self):
+        yield 'user_id', self.user_id
+        yield 'user_email', self.user_email
