924a79288f56805083bafaa78514f2a788bce103
jeong <jw.jeong@tilda.co.kr>
2024-06-30 16:22:35 +0900

    auth, user 간의 의존성 재적립

--- app/domains/auth/application/auth_service.py
+++ app/domains/auth/application/auth_service.py
@@ -1,45 +1,45 @@
-from __future__ import annotations
 from datetime import datetime, timedelta, timezone
+from app.domains.auth.domain.token import TokenPayload
+from app.domains.users.application.user_service import UserService
 from fastapi import Depends
-from typing import TYPE_CHECKING
-
-from app.configs.app_config import get_algorithm, get_token_secret_key
+from app.configs.app_config import get_algorithm, get_token_expire_minutes, get_token_secret_key
 from passlib.context import CryptContext
 import jwt
 
-if TYPE_CHECKING:
-    from app.domains.users.application.user_service import UserService
+from app.domains.users.infra.user_entity import UserEntity
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 ALGORITHM = get_algorithm()
 SECRET_KEY = get_token_secret_key()
+ACCESS_TOKEN_EXPIRE_MINUTES = get_token_expire_minutes()
 
 class AuthService:
-    def __init__(self):
-        self.user_service: 'UserService' = Depends('UserService')
+    def __init__(
+        self,
+        user_service: UserService = Depends(UserService)
+    ):
+        self.user_service: UserService = user_service
+        pass
 
-    async def authenticate_user(self, email: str, password: str):
-        user = await self.user_service._get_user_or_none(email=email)
+    async def authenticate_user(self, email: str, password: str) -> UserEntity:
+        user = await self.user_service.get_user_or_none(email=email)
         if not user:
             return False
-        if not self._verify_password(password, user.password):
+        if not self.verify_password(password, user.password):
             return False
         return user
 
-
-    def create_access_token(data: dict, expires_delta: timedelta| None = None):
-        to_encode = data.copy()
-        if expires_delta:
-            expire = datetime.now(timezone.utc) + expires_delta
-        else:
-            expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    def create_access_token(self, token_payload: TokenPayload):
+        to_encode = dict(token_payload)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         to_encode.update({"exp": expire})
+        
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt
     
-    def _verify_password(plain_password:str, hashed_password:str):
+    def verify_password(self, plain_password:str, hashed_password:str):
         return pwd_context.verify(plain_password, hashed_password)
 
-    def _get_password_hash(password:str):
+    def get_password_hash(self, password:str) -> str:
         return pwd_context.hash(password)
--- app/domains/users/application/user_service.py
+++ app/domains/users/application/user_service.py
@@ -1,30 +1,25 @@
-from __future__ import annotations
-from fastapi import Depends
-from typing import TYPE_CHECKING
+from fastapi import Depends, HTTPException
 
 from app.domains.users.infra.user_entity import UserEntity
 from app.domains.users.infra.user_repository import UserRepository
 
-if TYPE_CHECKING:
-    from app.domains.auth.application.auth_service import AuthService
-
 class UserService:
     def __init__(
         self, 
-        user_repository: UserRepository = Depends(UserRepository),
-        auth_service: 'AuthService' = Depends('AuthService'),
+        user_repository : UserRepository = Depends(UserRepository),
     ):
-        self.user_repository:UserRepository = user_repository
-        self.auth_servcie:'AuthService' = auth_service
-
-    async def _get_user_or_none(self, email:str) -> UserEntity | None:
+        self.user_repository :UserRepository = user_repository
+    
+    async def get_user_or_none(self, email:str) -> UserEntity | None:
         user: UserEntity | None = await self.user_repository.get_user_or_none(email = email)
 
         return user
 
-    async def create_user(self, email:str, password:str):
-
-        hashed_password = self.auth_servcie._get_password_hash(password)
-        user = await self.user_repository.create_user(email=email, password=hashed_password)
+    async def create_user(self, email:str, hashed_password:str) -> UserEntity:
+        existed_user = await self.get_user_or_none(email=email)
+        if existed_user:
+            raise HTTPException(status_code=409, detail="중복 유저가 존재합니다")
         
-        await user
+        user = await self.user_repository.create_user(email=email, password=hashed_password)
+
+        return user
