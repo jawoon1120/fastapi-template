e3c1f071387e4a2c5e9f525a21b7dd24587f4ee0
jeong <jw.jeong@tilda.co.kr>
2024-06-30 16:25:08 +0900

    책 조회 API 수정

--- app/domains/books/presentation/book_controller.py
+++ app/domains/books/presentation/book_controller.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, status
 
+from app.domains.auth.infra.auth_required import AuthRequired
+
 from ..application.book_service import BookService
 from ..presentation.dto.get_books_res import EnquiryDetail
 
@@ -9,7 +11,8 @@ router = APIRouter(tags=["book"])
 @router.get(
     "/books", 
     response_model=list[EnquiryDetail], 
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(AuthRequired())]
 )
 async def get_books(
     book_service : Annotated[BookService, Depends(BookService)]
@@ -20,7 +23,8 @@ async def get_books(
 
 @router.post(
     "/books", 
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(AuthRequired())]
 )
 async def create_books_with_transaction(
     book_service : Annotated[BookService, Depends(BookService)]
